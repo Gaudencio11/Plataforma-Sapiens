37511873761af4f7214035452c48327ddb47f808
Gaudencio11 <marcos.otavio10@gmail.com>
2021-06-07 16:50:22 -0300

    3 teste envio de email

--- pagina_pricipal/views.py
+++ pagina_pricipal/views.py
@@ -19,7 +19,10 @@ def menuView(request):
     if not request.user.is_authenticated:
         return redirect('/') 
 
-    """send_mail('Primeiro django test mail', 'Se você está recebendo esse email é porquê você conseguiu', 'marcos.teste083@gmail.com', ['marcos.otavio10@gmail.com'], fail_silently=False)"""
+
+    send_mail('Primeiro django test mail', 'Se você está recebendo esse email é porquê você conseguiu', 'marcos.teste083@gmail.com', ['marcos.otavio10@gmail.com',])
+
+
 
     """if AutorizedCpfs.objects.all() == True:
         lista_bd = AutorizedCpfs.objects.all()
